2862b3aa591698adcaf2baa210217c58df1c44da
Stephen Trewick <stephent818@gmail.com>
2019-06-30 14:19:44 -0700

    looping in jsx

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import Ticket from './Ticket';
 
+var masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'Firebase won\'t save record. Halp.'
+  },
+  {
+    names: 'Skeeter and Doug',
+    location: '4B',
+    issue: 'Fox image not displaying.'
+  },
+  {
+    names: 'Imani and Jacob',
+    location: '9F',
+    issue: 'Donkey picture not displaying on hover.'
+  }
+]
+
 function TicketList() {
   return(
     <div>
-      <h3>Queue List</h3>
-      <Ticket
-        location="3A"
-        names="Thato and Haley"
-        issue="Firebase will not save record."/>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index} />
+      )}
     </div>
   );
 }
